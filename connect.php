fb3b5bd932ee03b1826106e409458fcd732ff1e7
Sean Gbadamosi <139792459+seangbadamosi@users.noreply.github.com>
2024-11-25 13:47:02 -0800

    added cards

--- connect.php
+++ connect.php
@@ -7,12 +7,12 @@ $database = "vid_db";
 
 $connect = mysqli_connect($host_name, $username, $password, $database);
 
-if ( !$connect ) {
-    echo "could not connect" . mysqli_connect_error();
-}
+// if ( !$connect ) {
+//     echo "could not connect" . mysqli_connect_error();
+// }
 
-else {
-    echo "connection successful";
-}
+// else {
+//     echo "connection successful";
+// }
 
 ?>
