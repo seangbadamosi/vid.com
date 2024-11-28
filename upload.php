1f8409af7df2295862a360a8711650e4bf7cd51f
Sean Gbadamosi <139792459+seangbadamosi@users.noreply.github.com>
2024-11-28 12:14:42 -0800

    added form

---
+++ upload.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Compiled and minified CSS -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Document</title>
+</head>
+<body>
+  <div class="container">
+    <form action="">
+    <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col l6">
+          <label for="first_name">Vid Title</label><br>
+          <input placeholder="" id="first_name" type="text" class="validate">
+        </div>
+        <div class="input-field col l6">
+          <label for="first_name">Vid link</label><br>
+          <input placeholder="" id="first_name" type="text" class="validate">
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col l6">
+          <label for="first_name">Vid Description</label><br>
+          <input placeholder="" id="first_name" type="text" class="validate">
+        </div>
+        <div class="input-field col l6">
+          <label for="first_name">Vid id url</label><br>
+          <input placeholder="" id="first_name" type="text" class="validate">
+        </div>
+        <div class="center">
+          <a class="waves-effect waves-light btn-small"><i class="material-icons left">cloud_upload</i>button</a>
+        </div>
+      </div>
+
+    </form>
+    </div>
+</body>
+</html>
